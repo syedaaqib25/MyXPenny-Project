8346a0c3491626fa5e77f567a713f35853c67aa5
Syed Aaqib <114826060+syedaaqib25@users.noreply.github.com>
2025-02-23 00:33:15 +0530

    Create input.jsx

---
+++ src/components/ui/input.jsx
@@ -0,0 +1,17 @@
+// components/ui/Input.jsx
+
+import React from 'react';
+
+const Input = ({ type, placeholder, value, onChange }) => {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className="input"
+    />
+  );
+};
+
+export default Input;
